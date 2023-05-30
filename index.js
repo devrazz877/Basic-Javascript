1bfc843008a76bfad050256576c2c532a1ba641c
devrazz887 <devrazzpokhrel887@gmail.com>
2023-05-30 15:50:52 +0545

    add basic javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+const length = prompt("enter length");
+const width = prompt("enter width");
+const area = length*width;
+console.log(area)
+alert("the area is  : "+area);
+document.write("the area of rec is :" +area);
+
+
