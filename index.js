8c468210a836949923d28e740316ffaa257d6a73
devrazz887 <devrazzpokhrel887@gmail.com>
2023-06-01 16:37:50 +0545

    loop array object

--- index.js
+++ index.js
@@ -1,8 +1,314 @@
-const length = prompt("enter length");
-const width = prompt("enter width");
-const area = length*width;
-console.log(area)
-alert("the area is  : "+area);
-document.write("the area of rec is :" +area);
+// // // // // // // // // // // // // // // // // // // // // // // // 
+// // // // // // // // // // // // // // // // // // // // // // // const z = 78;
+// // // // // // // // // // // // // // // // // // // // // // // console.log(z);
+
+// // // // // // // // // // // // // // // // // // // // // // // z=90;
+// // // // // // // // // // // // // // // // // // // // // // // console.log(z);
+
+// // // // // // // // // // // // // // // // // // // // // // const name = "dev pokh";
+// // // // // // // // // // // // // // // // // // // // // // const y = 67;
+// // // // // // // // // // // // // // // // // // // // // // const ans = name + y;
+// // // // // // // // // // // // // // // // // // // // // // console.log(ans);
+
+// // // // // // // // // // // // // // // // // // // // // let y = true;
+// // // // // // // // // // // // // // // // // // // // // console.log(typeof y);
+
+// // // // // // // // // // // // // // // // // // // // let x = 56;
+// // // // // // // // // // // // // // // // // // // // let y = 78;
+// // // // // // // // // // // // // // // // // // // // let z = true ;
+// // // // // // // // // // // // // // // // // // // // let ans = x+y+z;
+// // // // // // // // // // // // // // // // // // // // console.log(ans);
+
+// // // // // // // // // // // // // // // // // // // let y = null;
+// // // // // // // // // // // // // // // // // // // console.log(typeof y);
+
+// // // // // // // // // // // // // // // // // // const name = "Harka";
+// // // // // // // // // // // // // // // // // // const lname = "rai";
+
+// // // // // // // // // // // // // // // // // // console.log(`haina k ho yesto yo thukaaaaaaa: ${name} ${lname}`);
 
 
+// // // // // // // // // // // // // // // // // const name = "dev";
+// // // // // // // // // // // // // // // // // const lname = "pokh";
+// // // // // // // // // // // // // // // // // const age  = 22;
+// // // // // // // // // // // // // // // // // console.log(`my name is ${name} ${lname}. My age is ${age} `);
+
+
+// // // // // // // // // // // // // // // // //type conversion 
+
+
+// // // // // // // // // // // // // // // // const x = 90
+// // // // // // // // // // // // // // // // console.log(x)
+// // // // // // // // // // // // // // // // console.log(typeof x)
+// // // // // // // // // // // // // // // // console.log(typeof String(x))
+
+
+// // // // // // // // // // // // // // //  const x = "hello world"
+// // // // // // // // // // // // // // //  console.log(x)
+// // // // // // // // // // // // // // //  console.log(typeof x)
+// // // // // // // // // // // // // // //   console.log(typeof Number(x))
+
+
+// // // // // // // // // // // // // // //strings methods
+
+
+// // // // // // // // // // //   let result = "hello world"
+// // // // // // // // // // // // // // console.log(result.length)
+
+// // // // // // // // // // // // // //concat
+// // // // // // // // // // // // // console.log(result.concat(" from dev"))
+// // // // // // // // // // // // //replace
+// // // // // // // // // // // // // console.log(result.replace("hello","burn"))
+
+// // // // // // // // // // // // //split
+// // // // // // // // // // // // //console.log(result.split(" "))
+
+// // // // // // // // // // // // //charat
+// // // // // // // // // // // // //console.log(result.charAt(7))
+
+// // // // // // // // // // // // //indexof
+// // // // // // // // // // // // //console.log(result.indexOf("w"))
+
+// // // // // // // // // // // // //includes
+// // // // // // // // // // // // //console.log(result.includes("world"))
+
+// // // // // // // // // // // // //trim(space udauncha database ma)
+// // // // // // // // // // // // //console.log(result.trim())
+
+// // // // // // // // // // // // //substring(kaha samma rakhney letter haru)
+// // // // // // // // // // // // console.log(result.substring(0,4))
+// // // // // // // // // // // // console.log(result.slice(0,4))
+
+// // // // // // // // // // // console.log(result.toLowerCase())
+// // // // // // // // // // // console.log(result.toUpperCase())
+
+
+
+// // // // // // // // // // //if else
+
+// // // // // // // // // // let email= "devrazzpokhrel887.com"
+// // // // // // // // // // let password="1111111"
+
+// // // // // // // // // // if(email === "devrazzpokhrel887.com"  && password === "1111111")
+// // // // // // // // // // {
+// // // // // // // // // //     console.log("welcome to the page")
+
+// // // // // // // // // // }
+// // // // // // // // // // else{
+// // // // // // // // // //     console.log("invalid")
+// // // // // // // // // // }
+
+// // // // // // // // // let day  = "cloudy"
+// // // // // // // // // if(day === "rainy")
+// // // // // // // // // {
+// // // // // // // // //     console.log("its a rainy day")
+// // // // // // // // // }
+// // // // // // // // // else if(day=== "cloudy"){
+// // // // // // // // //     console.log("k aja pani parla ta")
+// // // // // // // // // }
+// // // // // // // // // else if(day === "winter")
+// // // // // // // // // {
+// // // // // // // // //     console.log("tyo thik cha")
+
+// // // // // // // // // }
+// // // // // // // // // else{
+// // // // // // // // //     console.log("thik cja")
+// // // // // // // // // }
+
+// // // // // // // // let x = 10
+// // // // // // // // if(x>15)
+// // // // // // // // {
+// // // // // // // //     console.log("x is greater than 15")
+// // // // // // // // }
+// // // // // // // // else if(x>5)
+// // // // // // // // {
+// // // // // // // //     console.log("x is greater than 5")
+// // // // // // // // }
+// // // // // // // // else
+// // // // // // // // {
+// // // // // // // //     console.log("x is not greater than 15")
+// // // // // // // // }
+
+
+
+// // // // // // let grade = "c"
+// // // // // // if(grade === "A")
+// // // // // // {
+// // // // // //     console.log("keep it up man")
+// // // // // // }
+// // // // // // else if(grade === "B")
+// // // // // // {
+// // // // // //     console.log("ajai mehenat garna paryo")
+// // // // // // }
+// // // // // // else if(grade ==="C"){
+// // // // // //     console.log("thikai cha")
+// // // // // // }
+// // // // // // else if(grade === "D")
+// // // // // // {
+// // // // // // console.log("barbadd")
+// // // // // // }
+// // // // // // else{
+// // // // // //     console.log("thukkaaa")
+// // // // // // }
+
+// // // // // //switch(if else lekhi rakhnu pardaina mathi ko her ani tahahuncha)
+// // // // // let day = new Date().getDay();
+// // // // // switch (day) {
+// // // // //     case 0:
+// // // // //         console.log("today is sunday");
+        
+// // // // //         break;
+// // // // //     case 1:
+// // // // //             console.log("today is monday");
+// // // // //             break;
+// // // // //     case 2:
+// // // // //                 console.log("today is tuesday");
+// // // // //                 break;
+// // // // //     case 3:
+// // // // //                     console.log("today is wednesday");
+// // // // //                     break;
+// // // // //      case 4:
+// // // // //                         console.log("today is thursday");
+// // // // //                         break;
+// // // // //     case 5:
+// // // // //                             console.log("today is friday ");
+
+// // // // //     default:
+// // // // //         console.log("invalid")
+// // // // //         break;
+// // // // // }
+
+// // // // let month = "june"
+// // // // switch (month) {
+// // // //     case "jan":
+
+        
+// // // //         break;
+// // // //     case "feb":
+// // // //         break;
+// // // //     case "march":
+// // // //         console.log("the month is 1");
+// // // //         break;
+// // // //     case "april":
+// // // //     case "may":
+// // // //     case "june":
+// // // //     console.log("the monthe is 2");
+// // // //     break;
+// // // //     case "july":
+// // // //     case "aug":
+// // // //         case"sept":
+// // // //         console.log("the month is 3");
+// // // //         break;
+// // // //     default:
+// // // //         console.log("invalid")
+// // // //         break;
+
+// // // // }
+
+// // // // let grade = "z"
+// // // // switch (grade) {
+// // // //     case "A":
+// // // //         console.log("nice grade");
+// // // //         break;
+// // // //         case "B":
+// // // //             console.log("good grade");
+// // // //             break;
+// // // //             case "C":
+// // // //                 console.log("thikai grade");
+// // // //                 break;
+// // // //                 case "D":
+// // // //                     console.log("kharab grade");
+// // // //                     break;
+
+
+// // // //     default:
+// // // //         console.log("invalid ")
+// // // //         break;
+// // // // }
+
+// // // console.log(Math.random());
+// // // //loop
+
+
+// // // for(let i=0; i<10;i++)
+// // // {
+// // //     console.log("Iteration:",i);
+// // // }
+
+// // for(let i=0; i<10;i++)
+// //  {
+// //     if(i%2!==0)
+// //      console.log("Iteration:",i);
+// //  }
+
+// // let inputvalue = prompt("enter your number");
+// // let number = Number(inputvalue);
+// // if(isNaN(number))
+// // {
+// //     alert("enter valid number");
+// // }
+// // else
+// // {
+// //     document.write("odd number are:")
+
+// // for(let i=0; i<10;i++)
+// //  {
+// //      if(i%2!==0)
+// //      { document.write("<br>",i);
+    
+// //     }
+      
+
+// //  }
+// // }
+
+// //while loop 
+// // let i=0
+// // while (i<10) {
+// //     console.log("Iteration:" , i);
+// //     i++;
+// // }
+
+// //do while
+
+// // let i=0
+// // do {
+// //     console.log("hello world");
+// //     i++;
+// // } while (i<10);
+
+
+// //ARRAY
+
+// // const arr =[1,2,3,4,"ram",7,8,9]
+// // console.log(arr[4]);
+
+// // const arr = [];
+// // arr[0]=90;
+// // arr[1]=9;
+// // arr[2]=900;
+// // console.log(arr);
+
+// const arr = [3,5,7,8,9,0,1]
+// //console.log(arr.length)
+
+// //arr.push(10)
+// //arr.pop();
+// //arr.shift();
+// //arr.unshift(55);
+// // const result = arr.includes(5)
+// // console.log(arr);
+// //const result = arr.slice(1,5);
+// //console.log(arr)
+// console.log(result);
+
+
+//object
+
+const obj = 
+{
+    name : "dev",
+    age :22,
+    sex : "never",
+};
+console.log(obj.)
